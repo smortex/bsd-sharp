35b66553c7cb1f7757953dece34ed87e0d33b7a6
romain.tartiere <romain.tartiere@d7bd326d-7333-0410-8c1b-29977c593562>
2008-12-21 23:44:57 +0000

    lang/mono: Update to 2.2.

--- trunk/lang/mono/files/patch-mono_metadata_class.c
+++ trunk/lang/mono/files/patch-mono_metadata_class.c
@@ -1,14 +1,17 @@
---- mono/metadata/class.c.orig	2008-11-01 01:58:37.000000000 -0300
-+++ mono/metadata/class.c	2008-11-01 02:01:35.000000000 -0300
-@@ -7449,7 +7449,10 @@
+
+$FreeBSD$
+
+--- mono/metadata/class.c.orig
++++ mono/metadata/class.c
+@@ -7522,7 +7522,10 @@
  gboolean
  mono_class_generic_sharing_enabled (MonoClass *class)
  {
--#if defined(__i386__) || defined(__x86_64__)
+-#if defined(__i386__) || defined(__x86_64__) || defined(__arm__) || defined(__ppc__) || defined(__powerpc__)
 +#if defined(__FreeBSD__) && defined(__x86_64__)
-+/* https://bugzilla.novell.com/show_bug.cgi?id=434457 */
++	/* https://bugzilla.novell.com/show_bug.cgi?id=434457 */
 +	static gboolean supported = FALSE;
-+#elif defined(__i386__) || defined(__x86_64__)
++#elif defined(__i386__) || defined(__x86_64__) || defined(__arm__) || defined(__ppc__) || defined(__powerpc__)
  	static gboolean supported = TRUE;
  #else
  	/* Not supported by the JIT backends */
