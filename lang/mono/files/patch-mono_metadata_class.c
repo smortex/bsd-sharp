009c9858ff57b8cc0584b823c98f1bdccaa6c24e
Romain Tartière <romain@blogreen.org>
2009-01-27 19:57:41 +0000

    Hello mono-2.4 RC1!
    Switched to the preview MASTER_SITES and updated to 2.4.

--- lang/mono/files/patch-mono_metadata_class.c
+++ lang/mono/files/patch-mono_metadata_class.c
@@ -3,11 +3,11 @@ $FreeBSD$
 
 --- mono/metadata/class.c.orig
 +++ mono/metadata/class.c
-@@ -7522,7 +7522,10 @@
+@@ -7648,7 +7648,10 @@
  gboolean
  mono_class_generic_sharing_enabled (MonoClass *class)
  {
--#if defined(__i386__) || defined(__x86_64__) || defined(__arm__) || defined(__ppc__) || defined(__powerpc__)
+-#if defined(__i386__) || defined(__x86_64__) || defined(__arm__) || defined(__mono_ppc__)
 +#if defined(__FreeBSD__) && defined(__x86_64__)
 +	/* https://bugzilla.novell.com/show_bug.cgi?id=434457 */
 +	static gboolean supported = FALSE;
