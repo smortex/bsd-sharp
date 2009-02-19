8ab7dfd760e2ce74f034a962d12bdefe4f05d952
romain.tartiere <romain.tartiere@d7bd326d-7333-0410-8c1b-29977c593562>
2009-02-19 15:11:17 +0000

    devel/monodevelop: Update to 1.9.2 aka Beta 1.

--- trunk/devel/monodevelop/files/patch-SystemAssemblyService.cs
+++ trunk/devel/monodevelop/files/patch-SystemAssemblyService.cs
@@ -1,14 +1,13 @@
---- src/core/MonoDevelop.Core/MonoDevelop.Core/SystemAssemblyService.cs.orig	2008-12-22 20:20:38.000000000 -0300
-+++ src/core/MonoDevelop.Core/MonoDevelop.Core/SystemAssemblyService.cs	2008-12-22 20:21:26.000000000 -0300
-@@ -359,9 +359,9 @@
- 				foreach (string pathdir in path_dirs.Split (Path.PathSeparator)) {
- 					if (pathdir == null)
- 						continue;
--					if (File.Exists (pathdir + Path.DirectorySeparatorChar + "pkg-config")) {
-+					if (Directory.Exists (pathdir + Path.DirectorySeparatorChar + "../libdata/pkgconfig")) {
- 						libpath = Path.Combine(pathdir,"..");
--						libpath = Path.Combine(libpath,"lib");
-+						libpath = Path.Combine(libpath,"libdata");
- 						libpath = Path.Combine(libpath,"pkgconfig");
- 						break;
- 					}
+
+$FreeBSD$
+
+--- src/core/MonoDevelop.Core/MonoDevelop.Core/SystemAssemblyService.cs.orig
++++ src/core/MonoDevelop.Core/MonoDevelop.Core/SystemAssemblyService.cs
+@@ -412,6 +412,7 @@
+ 			
+ 			string[] suffixes = new string [] {
+ 				Path.Combine ("lib", "pkgconfig"),
++				Path.Combine ("libdata", "pkgconfig"),
+ 				Path.Combine ("lib64", "pkgconfig"),
+ 				Path.Combine ("share", "pkgconfig"),
+ 			};
