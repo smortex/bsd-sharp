cfe4ed6950d1ce792b1b7a5e701c60919ab66a56
Romain Tartière <romain@blogreen.org>
2009-03-13 10:46:21 +0000

    lang/mono: Sync mono/metadata/class.c patch with upstream.
    Some pre-compiler macros have been updated and __ppc__ / __powerpc__ have been
    replaced with __mono_ppc__.  Update patch accordingly so that it keeps
    consistent.

--- lang/mono/files/patch-mono_metadata_class.c
+++ lang/mono/files/patch-mono_metadata_class.c
@@ -11,7 +11,7 @@ $FreeBSD$
 +#if defined(__FreeBSD__) && defined(__x86_64__)
 +	/* https://bugzilla.novell.com/show_bug.cgi?id=434457 */
 +	static gboolean supported = FALSE;
-+#elif defined(__i386__) || defined(__x86_64__) || defined(__arm__) || defined(__ppc__) || defined(__powerpc__)
++#elif defined(__i386__) || defined(__x86_64__) || defined(__arm__) || defined(__mono_ppc__)
  	static gboolean supported = TRUE;
  #else
  	/* Not supported by the JIT backends */
